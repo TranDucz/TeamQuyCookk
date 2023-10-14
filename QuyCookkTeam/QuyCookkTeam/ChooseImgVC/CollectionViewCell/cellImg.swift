5768ff18eb7719892298041caede5bc99aa7b93e
duytc <duytc@vnpay.vn>
2023-10-14 13:41:50 +0700

    [DEV] collectionview

--- QuyCookkTeam/QuyCookkTeam/ChooseImgVC/CollectionViewCell/cellImg.swift
+++ QuyCookkTeam/QuyCookkTeam/ChooseImgVC/CollectionViewCell/cellImg.swift
@@ -9,9 +9,17 @@ import UIKit
 
 class cellImg: UICollectionViewCell {
 
+    @IBOutlet weak var vImg: UIImageView!
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
 
+    func bindingData(img: UIImage) {
+        vImg.image = img
+        
+    }
+    @IBAction func selectAction(_ sender: Any) {
+        print("Selected")
+    }
 }
