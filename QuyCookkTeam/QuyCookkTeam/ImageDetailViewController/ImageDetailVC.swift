e8c323670471ed81c7cfc4ca7e9251ed7a18ab4b
Hartzed Story <ducnafs1579@gmail.com>
2023-10-14 11:09:03 +0700

    backup

--- QuyCookkTeam/QuyCookkTeam/ImageDetailViewController/ImageDetailVC.swift
+++ QuyCookkTeam/QuyCookkTeam/ImageDetailViewController/ImageDetailVC.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class ImageDetailVC: UIViewController {
     @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var smallImageCollectionView: UICollectionView!
     @IBOutlet weak var vSmallImageContainer: UIView!
     @IBOutlet weak var vContent: UIView!
     var dataSource:[UIImage] = []
@@ -21,22 +22,40 @@ class ImageDetailVC: UIViewController {
         collectionView.delegate = self
         collectionView.dataSource = self
         collectionView.register(UINib(nibName: "ImageCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "imageCell")
+        
+        smallImageCollectionView.delegate = self
+        smallImageCollectionView.dataSource = self
+        smallImageCollectionView.register(UINib(nibName: "ImageCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "smallImageCell")
     }
 }
 extension ImageDetailVC: UICollectionViewDelegate, UICollectionViewDataSource,UICollectionViewDelegateFlowLayout{
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 1
+        if collectionView == smallImageCollectionView {
+            return 10
+        } else {
+            return 1
+        }
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let imageCell = collectionView.dequeueReusableCell(withReuseIdentifier: "imageCell", for: indexPath) as! ImageCollectionViewCell
-        return imageCell
+        if collectionView == smallImageCollectionView {
+            let smallImage = collectionView.dequeueReusableCell(withReuseIdentifier: "smallImageCell", for: indexPath) as! ImageCollectionViewCell
+            return smallImage
+        } else {
+            let imageCell = collectionView.dequeueReusableCell(withReuseIdentifier: "imageCell", for: indexPath) as! ImageCollectionViewCell
+            return imageCell
+        }
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let height = vContent.frame.height
-        let width = UIScreen.main.bounds.width
-        return CGSize(width: width, height: height)
+        if collectionView == smallImageCollectionView {
+            let height = vSmallImageContainer.frame.height
+            let width = height * 1.5
+            return CGSize(width: width, height: height)
+        } else {
+            let height = vContent.frame.height
+            let width = UIScreen.main.bounds.width
+            return CGSize(width: width, height: height)
+        }
     }
-    
 }
